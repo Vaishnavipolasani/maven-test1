22d54c59dc1fb3f3482f0901d8f2a0e35d27f0fa
vaishnavi <polsanivaishnavi@gmail.com>
2025-09-13 11:52:10 +0530

    VAISHNAVIP0001 - Added checkEvenOdd function and my name function in Utility.java

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,16 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    // Function to check even or odd
+    public static String checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+    // My name function
+    public static void vaishnavi() {
+        System.out.println("I am Vaishnavi");
+    }
+}
